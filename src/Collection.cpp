b48d7a8692bce68397635cf8cc6e3009975e8ee5
virginia4 <apostolop.virginia@gmail.com>
2023-03-14 11:46:55 +0100

    Functions addModel, addData and addModelDataPair have been added to the Collection. You can use Collection to directly initialise values on each of these tables

--- src/Collection.cpp
+++ src/Collection.cpp
@@ -1,11 +1,20 @@
 // Collection.cpp
-
+#include <iostream>
 #include "Collection.h"
+#include "ModelDataPair.h"
+#include "PData.h"
+#include "PModel.h"
+#include "EnumTables.h"
+
+
+
 using namespace mulch;
 
 Collection::Collection()
 {
-
+	_model = new PModel();	
+	_data = new PData();
+	_mdp = new ModelDataPair();
 }
 
 std::string Collection::insertQuery()
@@ -18,6 +27,7 @@ std::string Collection::insertQuery()
 std::string Collection::updateQuery()
 {
 	std::string query;
+	// fixed = true or false vlaue, collectiontion_id = sql_id
 	query = "UPDATE Collection SET fixed = 'blah' WHERE Collection_ID = 4;";
 
 	return query;
@@ -31,4 +41,26 @@ std::string Collection::selectPidQuery()
 
 	return query;
 }
+
+void Collection::addModel(RepresentationEnum rep, std::string pdbName)
+{
+	_model->setRepType(rep);
+	_model->setFileName(pdbName);
+}
+
+void Collection::addData(DataEnum datatype, std::string datafile)
+{
+	_data->setFileName(datafile);
+	_data->setDataInfo(datatype);
+}
+
+
+void Collection::addModelDataPair(RepresentationEnum rep, std::string pdbName, DataEnum datatype, std::string datafile)
+{
+	_mdp->setRep(rep);
+	_mdp->setFile(pdbName);
+	_mdp->setDataType(datatype);
+	_mdp->setDataFile(datafile);
+}
+
 
--- src/Collection.h
+++ src/Collection.h
@@ -6,22 +6,36 @@
 
 #include "Object.h"
 #include "Database.h"
+// #include "EnumTables.h"
 
 namespace mulch
 {
+	class PModel;
+	class PData;
+	class ModelDataPair;
 	class Collection: public Object
 	{
 	public:
 		Collection();
+		void addModel(RepresentationEnum rep, std::string pdbName);
+		void addData(DataEnum datatype, std::string datafile);
+		void addModelDataPair(RepresentationEnum rep, std::string pdbName, DataEnum datatype, std::string datafile);
+
 	protected:
 		virtual std::string insertQuery();
 		virtual std::string updateQuery();
 		virtual std::string selectPidQuery();
 	private:
+		bool _fixed; 
+		std::vector<ModelDataPair*> _vectormdp ;
 		virtual std::string sqlIdName()
 		{
 			return "collection_id";
 		}
+		PModel *_model = nullptr;
+		PData *_data = nullptr;
+		ModelDataPair *_mdp = nullptr;
+
 	};
 }
 
