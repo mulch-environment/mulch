5fea85272ddf7bec9ddf3f10de74f395b4e3901e
virginia4 <apostolop.virginia@gmail.com>
2023-04-10 22:59:44 +0200

    Refactor the RepresentationType class to allow updating a specific representation type in the database. The updateRepType function was added to update the foreign keys in the RepresentationType table based on the representation type being updated. The updateQuery function was updated to call the updateRepType function based on the representation type being updated. The updateDependenciesBefore function was updated to check if each representation type was not null before updating its dependencies in the database.

--- src/RepresentationType.cpp
+++ src/RepresentationType.cpp
@@ -11,10 +11,10 @@ using namespace mulch;
 
 RepresentationType::RepresentationType()
 {
-	_atomicModelInfo = new AtomicModelInfo();
-	_bondBasedModelInfo = new BondBasedModelInfo();
-	_coarseGrainingModelInfo = new CoarseGrainingModelInfo();
-	_ensembleRefineInfo = new EnsembleRefineInfo();	
+	_atomicModelInfo = nullptr;
+	_bondBasedModelInfo = nullptr;
+	_coarseGrainingModelInfo = nullptr;
+	_ensembleRefineInfo = nullptr;	
 }
 
 std::string RepresentationType::insertQuery()
@@ -25,16 +25,67 @@ std::string RepresentationType::insertQuery()
 	return query;
 }
 
+std::string RepresentationType::updateRepType(std::string repTypeIdName, int repTypeIdValue)
+{
+	std::string query;
+	query = "UPDATE RepresentationType SET ";
+	query += repTypeIdName; 
+	query += " = ";
+	query += std::to_string(repTypeIdValue); 
+	query += " WHERE representation_type_id = ";
+	query += std::to_string(primaryId());
+	query += ";";
+	Utility::protectsql(query);
+	return query;
+}
+
 std::string RepresentationType::updateQuery()
 {
 	std::string query;
-	query = "UPDATE RepresentationType SET bondbased_model_ID = 1 WHERE representation_type_ID = 3";
-	query += ";";
-	// query = "UPDATE RepresentationType SET atomic_model_ID= 1 WHERE representation_type_ID = 4";
-	// query += ";";
-	return query;
-	// return "";
+	std::string repTypeIdName;
+	int repTypeIdValue;
+
+	if (_type == Atomic)
+	{
+		repTypeIdName = "atomic_model_id";
+		repTypeIdValue = _atomicModelInfo->primaryId();
+		query = updateRepType(repTypeIdName, repTypeIdValue);
+		std::cout << query << std::endl;
+		return query;
+	}
+	else if (_type == BondBased)
+	{
+		repTypeIdName = "bondbased_model_id";
+		repTypeIdValue = _bondBasedModelInfo->primaryId();
+		query = updateRepType(repTypeIdName, repTypeIdValue);
+		std::cout << query << std::endl;
+		return query;
+	}
+	else if (_type == CG)
+	{
+		repTypeIdName = "coarsegraining_model_id";
+		repTypeIdValue = _coarseGrainingModelInfo->primaryId();
+		query = updateRepType(repTypeIdName, repTypeIdValue);
+		std::cout << query << std::endl;
+		return query;
+		
+	}
+		else if (_type == Ensemble)
+	{
+		repTypeIdName = "ensemble_refine_id";
+		repTypeIdValue = _ensembleRefineInfo->primaryId();
+		query = updateRepType(repTypeIdName, repTypeIdValue);
+		std::cout << query << std::endl;
+		return query;
+	}
+	else
+	{
+		throw std::runtime_error("Can't update RepresentationType: no representation type");
+	}
+
 }
+
+
 
 std::string RepresentationType::selectPidQuery()
 {
@@ -42,42 +93,65 @@ std::string RepresentationType::selectPidQuery()
 	query = "SELECT * FROM RepresentationType WHERE representation_type_ID = ";
 	query += std::to_string(primaryId());
 	query += ";";
-
+	Utility::protectsql(query);
 	return query;
 }
 
 
-void RepresentationType::updateDependencies(Database *db)
+void RepresentationType::updateDependenciesBefore(Database *db)
 {
 	/* Foreign keys (FK): if a column is assigned a FK, each row of that column 
 	MUST contain a value that exists in the foreigh column it references.
 	**/
-	_atomicModelInfo->updateDatabase(db);
-	_bondBasedModelInfo->updateDatabase(db);
-	_coarseGrainingModelInfo->updateDatabase(db);
-	_ensembleRefineInfo->updateDatabase(db);
+	if (_atomicModelInfo != nullptr)
+	{
+		std::cout << "Updating dependencies for RepresentationType->AtomicModelInfo \n" << std::endl;
+		_atomicModelInfo->updateDatabase(db);
+	}
+	else if (_bondBasedModelInfo != nullptr)
+	{
+		std::cout << "Updating dependencies for RepresentationType->BondBasedModelInfo \n" << std::endl;
+		_bondBasedModelInfo->updateDatabase(db);
+	}
+	else if (_coarseGrainingModelInfo != nullptr)
+	{
+		std::cout << "Updating dependencies for RepresentationType->CoarseGrainingModelInfo \n" << std::endl;
+		_coarseGrainingModelInfo->updateDatabase(db);
+	}
+	else if (_ensembleRefineInfo != nullptr)
+	{
+		std::cout << "Updating dependencies for RepresentationType->EnsembleRefineInfo \n" << std::endl;
+		_ensembleRefineInfo->updateDatabase(db);
+	}
+	else
+	{
+		throw std::runtime_error("Can't update dependencies for RepresentationType: no representation type");
+	}
 }
 
 void RepresentationType::setRepType(RepresentationEnum rep)
 {
-
 	MulchExceptions::RepTypeIsNone(_type);
 	_type = rep;
 	std::cout<<_type<<std::endl;
 	if (_type == Atomic)
 	{
+		std::cout<<"New instance for Atomistic model type"<<std::endl;
 		_atomicModelInfo = new AtomicModelInfo();
 	}
 	else if (_type == BondBased)
 	{
+		std::cout<<"New instance for Bond-based model type"<<std::endl;
 		_bondBasedModelInfo = new BondBasedModelInfo();
 	}
 	else if (_type == CG)
 	{
+		std::cout<<"New instance for CG model type"<<std::endl;
 		_coarseGrainingModelInfo = new CoarseGrainingModelInfo();
 	}
 	else if (_type == Ensemble)
 	{
+		std::cout<<"New instance for Ensemble model type"<<std::endl;
 		_ensembleRefineInfo = new EnsembleRefineInfo();
 	}
 
@@ -92,6 +166,27 @@ void RepresentationType::setFileName(std::string pdbName)
 	}
 }
 
+RepresentationType* RepresentationType::representationTypeByPrimaryId(int id, Database *db)
+{
+	RepresentationType *representationType = new RepresentationType();
+	representationType->retrieveExisting(id, db);
+}
 
+
+
+// ~RepresentationType() {
+//     if (_atomicModelInfo) {
+//         delete _atomicModelInfo;
+//     }
+//     if (_bondBasedModelInfo) {
+//         delete _bondBasedModelInfo;
+//     }
+//     if (_coarseGrainingModelInfo) {
+//         delete _coarseGrainingModelInfo;
+//     }
+//     if (_ensembleRefineInfo) {
+//         delete _ensembleRefineInfo;
+//     }
+// }
 
 
--- src/RepresentationType.h
+++ src/RepresentationType.h
@@ -6,6 +6,8 @@
 
 #include "Object.h"
 #include "RepresentationType.h"
+#include "Utility.h"
+
 namespace mulch
 {	
 	class AtomicModelInfo;
@@ -20,14 +22,18 @@ namespace mulch
 		virtual void setRepType(RepresentationEnum rep);
 		/* sends pdbName to AtomicInfo */
 		virtual void setFileName(std::string pdbName);
-
-
-	
+		// static void representationTypeByPrimaryId(std::string rep_id, Database *db);
+		static RepresentationType* representationTypeByPrimaryId(int id, Database *db);
+		static std::string staticSqlIDName()
+		{
+			return "representation_type_id";
+		}
 	protected:
 		virtual std::string insertQuery();
 		virtual std::string updateQuery();
 		virtual std::string selectPidQuery();
-		virtual void updateDependencies(Database *db);
+		virtual void updateDependenciesBefore(Database *db);
+		std::string updateRepType(std::string repTypeIdName, int repTypeIdValue);
 
 	private:
 		virtual std::string sqlIdName()
@@ -40,9 +46,7 @@ namespace mulch
 		BondBasedModelInfo* _bondBasedModelInfo = nullptr;
 		CoarseGrainingModelInfo* _coarseGrainingModelInfo = nullptr;
 		EnsembleRefineInfo* _ensembleRefineInfo = nullptr;
-		std::string _comments = "blah";
-		
-
+		std::string _comments = "No comments yet";
 
 		
 	};
