86adf40c451668d10161ad988c9e03f2187364f5
virginia4 <apostolop.virginia@gmail.com>
2023-04-10 19:19:18 +0200

    Add default comments value and update sqlIdName() function for CoarseGrainingModelInfo. - Added _comments member variable with default value of 'No comments yet' - Updated sqlIdName() function to return correct primary key column name

--- src/CollectionHasDataset.h
+++ src/CollectionHasDataset.h
@@ -1,5 +1,12 @@
-// Object (mirrored from database)
-// crystallographic Model will be a type of Object
+/* CollectionHasDataset:  implementation of the CollectionHasDataset class, 
+which is used to represent a collection of model and data pairs that belong 
+to a specific collection. CollectionHasDataset class connects a Collection 
+and a ModelDataPair object, and can have multiple instances associated with both.
+This means that a single Collection object can have multiple associated 
+ModelDataPair objects through CollectionHasDataset, and a single ModelDataPair 
+object can be associated with multiple Collection objects through 
+CollectionHasDataset. 
+*/	
 
 #ifndef __mulch__CollectionHasDatset_h__
 #define __mulch__CollectionHasDatset_h__
@@ -14,20 +21,43 @@ namespace mulch
 	class CollectionHasDataset : public Object
 	{
 	public:
+		std::vector<CollectionHasDataset*> chdVector;
 		CollectionHasDataset();
+		
+		/* Sets the _collection member variable of the 
+		current CollectionHasDataset object to the input 
+		collection onject */
+		void setCollection(Collection *_collection);
+		
+		/* Adds the input _mdp objets to the mdpVector 
+		variable of the current CollectionHasDataset object */
+		void setModelDataPair(ModelDataPair* _MDpair);
+
+		/* Get ModelDataPair form CollectionHasDataset */
+		virtual const ModelDataPair* getModelDataPair() const 
+		{
+        	return _modelDataPair;
+    	}
+
+    	/* Get Collection form CollectionHasDataset */
+    	virtual const Collection* getCollection() const 
+    	{
+    		return _collection;
+    	}
 
 	protected:
+
 		virtual std::string insertQuery();
 		virtual std::string updateQuery();
 		virtual std::string selectPidQuery();
-		virtual void updateDependencies(Database *db);
-
+		virtual void updateDependenciesBefore(Database *db);
+		void fillInFromResult(const Result &res);
 	private:
 		virtual std::string sqlIdName()
 		{
 			return "collectionhasdataset_id";
 		}	
-
+		std::vector<ModelDataPair*> mdpVector;
 		ModelDataPair *_modelDataPair = nullptr;
 		Collection *_collection = nullptr;
 
