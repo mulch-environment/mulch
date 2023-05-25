76f1e3a813257e342dc4013e8ea602fc1c7cf365
virginia4 <apostolop.virginia@gmail.com>
2023-05-25 14:48:20 +0200

    Newest version of template.sql

--- assets/template.sql
+++ assets/template.sql
@@ -1,12 +1,11 @@
-
-CREATE TABLE "Model"(
+CREATE TABLE IF NOT EXISTS "Model"(
   model_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
   representation_type_id INTEGER NOT NULL,
   structure_technique_id INTEGER NOT NULL,
-  pdb_code VARCHAR,
+  pdb_code TEXT,
   haspdb BOOLEAN,
-  comments VARCHAR,
-  creationtime DATETIME,
+  comments TEXT,
+  creation_time DATETIME,
   CONSTRAINT "StructureTechniqueInfo_Model"
     FOREIGN KEY (structure_technique_id)
       REFERENCES "StructureTechniqueInfo" (structure_technique_id)
@@ -16,11 +15,11 @@ CREATE TABLE "Model"(
       REFERENCES "RepresentationType" (representation_type_id) ON DELETE No action
       ON UPDATE No action
 );
-
-CREATE TABLE "RepresentationType"(
+CREATE TABLE sqlite_sequence(name,seq);
+CREATE TABLE IF NOT EXISTS "RepresentationType"(
   representation_type_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-  atomic_model_id INTEGER,
-  bondbased_model_id INTEGER,
+  atomic_model_id INTEGER, 
+  bondbased_model_id INTEGER, 
   coarsegraining_model_id INTEGER,
   ensemble_refine_id INTEGER,
   CONSTRAINT "RepresentationType_ak_1" UNIQUE(representation_type_id),
@@ -39,18 +38,15 @@ CREATE TABLE "RepresentationType"(
   CONSTRAINT "AtomicModelInfo_RepresentationType"
     FOREIGN KEY (atomic_model_id) REFERENCES "AtomicModelInfo" (atomic_model_id)
       ON DELETE No action ON UPDATE No action,
-                  CHECK ((bondbased_model_ID is NULL AND coarsegraining_model_ID is NULL 
-               AND ensemble_refine_ID is NULL) OR 
-                  (atomic_model_ID is NULL AND coarsegraining_model_ID is NULL 
-               AND ensemble_refine_ID is NULL) OR
-                   (atomic_model_ID is NULL AND bondbased_model_ID is NULL 
-               AND ensemble_refine_ID is NULL) OR 
-                   (atomic_model_ID is NULL AND bondbased_model_ID is NULL 
-               AND coarsegraining_model_ID is NULL)
+                  CHECK ((coarsegraining_model_id is NULL 
+               AND ensemble_refine_id is NULL) OR 
+                   (atomic_model_id is NULL AND bondbased_model_id is NULL 
+               AND ensemble_refine_id is NULL) OR 
+                   (atomic_model_id is NULL AND bondbased_model_id is NULL 
+               AND coarsegraining_model_id is NULL)
                    )
 );
-
-CREATE TABLE "StructureTechniqueInfo"(
+CREATE TABLE IF NOT EXISTS "StructureTechniqueInfo"(
   structure_technique_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
   crystallographic_info_id INTEGER,
   nmr_info_id INTEGER,
@@ -59,7 +55,7 @@ CREATE TABLE "StructureTechniqueInfo"(
   CONSTRAINT "CryoEMInfo_StructureTechniqueInfo"
     FOREIGN KEY (cryoem_info_id) REFERENCES "CryoEMInfo" (cryoem_info_id)
       ON DELETE No action ON UPDATE No action,
-  CONSTRAINT "CrystalographicInfo_StructureTechniqueInfo"
+  CONSTRAINT "CrystallographicInfo_StructureTechniqueInfo"
     FOREIGN KEY (crystallographic_info_id)
       REFERENCES "CrystallographicInfo" (crystallographic_info_id)
       ON DELETE No action ON UPDATE No action,
@@ -67,87 +63,58 @@ CREATE TABLE "StructureTechniqueInfo"(
     FOREIGN KEY (nmr_info_id) REFERENCES "NMRInfo" (nmr_info_id) ON DELETE No action
       ON UPDATE No action
 );
-
---------------------------------------
-CREATE TABLE "AtomicModelInfo"(
-  atomic_model_id INTEGER NOT NULL,
+CREATE TABLE IF NOT EXISTS "AtomicModelInfo"(
+  atomic_model_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
   tlsparameters_id INTEGER,
-  pdb_code BLOB,
-  "hasPDB" BLOB,
-  comments VARCHAR,
-  "pdbFIlesName" BLOB,
-  "pdbFilePath" BLOB,
-  PRIMARY KEY(atomic_model_id),
+  pdb_code VARCHAR,
+  haspdb BOOLEAN,
+  comments TEXT,
+  pdbFileName VARCHAR,
+  pdbFilePath VARCHAR,
   CONSTRAINT "AtomicModelInfo_ak_1" UNIQUE(atomic_model_id),
   CONSTRAINT "TLSParametersInfo_AtomicModelInfo"
     FOREIGN KEY (tlsparameters_id) REFERENCES "TLSParametersInfo" (tlsparameters_id)
       ON DELETE No action ON UPDATE No action
 );
-
-
-CREATE TABLE "BondBasedModelInfo"(
-  bondbased_model_id INTEGER NOT NULL,
-  comments VARCHAR,
-  PRIMARY KEY(bondbased_model_id),
+CREATE TABLE IF NOT EXISTS "BondBasedModelInfo"(
+  bondbased_model_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+  comments TEXT,
   CONSTRAINT "BondBasedModelInfo_ak_1" UNIQUE(bondbased_model_id)
 );
-
-CREATE TABLE "CoarseGrainingModelInfo"(
-  coarsegraining_model_id INTEGER NOT NULL,
-  comments VARCHAR,
-  PRIMARY KEY(coarsegraining_model_id),
+CREATE TABLE IF NOT EXISTS "CoarseGrainingModelInfo"(
+  coarsegraining_model_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+  comments TEXT,
   CONSTRAINT "CoarseGrainingModelInfo_ak_1" UNIQUE(coarsegraining_model_id)
 );
-
-CREATE TABLE "TLSParametersInfo"(
-  tlsparameters_id INTEGER NOT NULL,
-  comments VARCHAR,
-  PRIMARY KEY(tlsparameters_id),
+CREATE TABLE IF NOT EXISTS "TLSParametersInfo"(
+  tlsparameters_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+  comments TEXT,
   CONSTRAINT "TLSParametersInfo_ak_1" UNIQUE(tlsparameters_id)
 );
-
-CREATE TABLE "EnsembleRefineInfo"(
-  ensemble_refine_id INTEGER NOT NULL,
-  comments VARCHAR,
-  PRIMARY KEY(ensemble_refine_id),
+CREATE TABLE IF NOT EXISTS "EnsembleRefineInfo"(
+  ensemble_refine_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+  comments TEXT,
   CONSTRAINT "EnsembleRefineInfo_ak_1" UNIQUE(ensemble_refine_id)
 );
-
---------------------------------------
-CREATE TABLE "CrystallographicInfo"(
-  crystallographic_info_id INTEGER NOT NULL,
+CREATE TABLE IF NOT EXISTS "CrystallographicInfo"(
+  crystallographic_info_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
   serial_id INTEGER,
   neutron_id INTEGER,
   electron_id INTEGER,
-  comments VARCHAR,
-  PRIMARY KEY(crystallographic_info_id),
-  CONSTRAINT "CrystalographicInfo_ak_1" UNIQUE(crystallographic_info_id)
+  comments TEXT,
+  CONSTRAINT "CrystallographicInfo_ak_1" UNIQUE(crystallographic_info_id)
 );
-
-CREATE TABLE "NMRInfo"(
-  nmr_info_id INTEGER NOT NULL,
+CREATE TABLE IF NOT EXISTS "NMRInfo"(
+  nmr_info_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
   q_factor REAL,
   rdc REAL,
   rdcobs REAL,
   rdcmodel REAL,
   peak_snr REAL,
-  comments VARCHAR,
-  PRIMARY KEY(nmr_info_id),
+  comments TEXT,
   CONSTRAINT "NMRInfo_ak_1" UNIQUE(nmr_info_id)
 );
-
-CREATE TABLE "CryoEMInfo"(
-  cryoem_info_id INTEGER NOT NULL,
-  dos REAL,
-  rmsd REAL,
-  native_ovelap REAL,
-  comments VARCHAR,
-  PRIMARY KEY(cryoem_info_id),
-  CONSTRAINT "CryoEMInfo_ak_1" UNIQUE(cryoem_info_id)
-);
-
-
-CREATE TABLE "ModelDataPair"(
+CREATE TABLE IF NOT EXISTS "ModelDataPair"(
   modeldatapair_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
   data_id INTEGER,
   model_id INTEGER,
@@ -155,12 +122,12 @@ CREATE TABLE "ModelDataPair"(
     FOREIGN KEY (data_id) REFERENCES "Data" (data_id),
   CONSTRAINT "Model_Dataset" FOREIGN KEY (model_id) REFERENCES "Model" (model_id)
 );
-
-CREATE TABLE "Data"(
+CREATE TABLE IF NOT EXISTS "Data"(
   data_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
   data_nmr_info_id INTEGER,
   data_crystallographic_info_id INTEGER,
   data_cryoem_info_id INTEGER,
+  comments TEXT,
   CONSTRAINT "CryoEMDataInfo_DataInfo"
     FOREIGN KEY (data_cryoem_info_id)
       REFERENCES "DataCryoEMInfo" (data_cryoem_info_id) ON DELETE No action
@@ -170,53 +137,109 @@ CREATE TABLE "Data"(
       REFERENCES "DataCrystallographicInfo" (data_crystallographic_info_id)
       ON DELETE No action ON UPDATE No action,
   CONSTRAINT "NMRDataInfo_DataInfo"
-    FOREIGN KEY (nmr_data_info_id) REFERENCES "DataNMRInfo" (data_nmr_info_id)
-      ON DELETE No action ON UPDATE No action
+    FOREIGN KEY (data_nmr_info_id) REFERENCES "DataNMRInfo" (data_nmr_info_id)
+      ON DELETE No action ON UPDATE No action,
+      CHECK ((data_nmr_info_id is NULL AND data_crystallographic_info_id is NULL) OR 
+             (data_crystallographic_info_id is NULL AND data_cryoem_info_id is NULL) OR 
+             (data_nmr_info_id is NULL AND data_cryoem_info_id is NULL))
 );
-
-CREATE TABLE "DataNMRInfo"(
+CREATE TABLE IF NOT EXISTS "DataNMRInfo"(
   data_nmr_info_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-  comments VARCHAR,
-  "nmrqualitydata_id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+  comments TEXT,
+  nmrqualitydata_id INTEGER NOT NULL,
   CONSTRAINT "NMRDataInfo_ak_1" UNIQUE(data_nmr_info_id),
   CONSTRAINT "NMRQualityData_DataNMRInfo"
     FOREIGN KEY ("nmrqualitydata_id")
       REFERENCES "NMRQualityData" (nmrqualitydata_id)
 );
-
-
-CREATE TABLE "DataCrystallographicInfo"(
+CREATE TABLE IF NOT EXISTS "DataCrystallographicInfo"(
   data_crystallographic_info_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-  crystalqualitydata_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  crystalqualitydata_id INTEGER,
   reflections_no INTEGER,
   wavelength REAL,
-  comments VARCHAR,
+  file_name TEXT,
+  comments TEXT,
   CONSTRAINT "CrystallographicDataInfo_ak_1" UNIQUE(data_crystallographic_info_id),
   CONSTRAINT "QualityData_DataCrystallographicInfo"
     FOREIGN KEY (crystalqualitydata_id)
       REFERENCES "CrystalQualityData" (crystalqualitydata_id)
 );
-
-CREATE TABLE "DataCryoEMInfo"(
+CREATE TABLE IF NOT EXISTS "CrystalQualityData"(
+  crystalqualitydata_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+  CChalf_average REAL,
+  CCstar REAL,
+  Rsplit REAL,
+  SNR REAL,
+  "CChalf_per_bin" INTEGER
+);
+CREATE TABLE IF NOT EXISTS "DataCryoEMInfo"(
   data_cryoem_info_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-  cryoemqualitydata_id INTEGER PRIMARY KEY AUTOINCREMENT,
-  comments VARCHAR,
+  cryoemqualitydata_id INTEGER,
+  comments TEXT,
   CONSTRAINT "CryoEMDataInfo_ak_1" UNIQUE(data_cryoem_info_id),
   CONSTRAINT "CryoemQualityData_DataCryoEMInfo"
     FOREIGN KEY (cryoemqualitydata_id)
       REFERENCES "CryoemQualityData" (cryoemqualitydata_id)
 );
-
-CREATE TABLE "Collection"
-  (collection_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, fixed BLOB);
-
-CREATE TABLE "CollectionHasDataset"(
+CREATE TABLE IF NOT EXISTS "Collection"(
+  Collection_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
+  fixed TEXT
+  );
+CREATE TABLE IF NOT EXISTS "CollectionHasDataset"(
   collectionhasdataset_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
   modeldatapair_id INTEGER NOT NULL,
   collection_id INTEGER NOT NULL,
   CONSTRAINT "Dataset_CollectionHadDataset"
-    FOREIGN KEY ("modeldatapair_id") REFERENCES "ModelDataPair" (modeldatapair_id),
+    FOREIGN KEY ("modeldatapair_id") REFERENCES "ModelDataPair" (modeldatapair_id)
   CONSTRAINT "Collection_CollectionHadDataset"
-    FOREIGN KEY (collection_id) REFERENCES "Collection" (Collection_id)
+    FOREIGN KEY ("Collection_id") REFERENCES "Collection" (collection_id)
 );
-
+CREATE TABLE IF NOT EXISTS "RfactorsInfo"(
+  rfactors_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+  modeldatapair_id INTEGER NOT NULL,
+  Rfree REAL,
+  Rwork REAL,
+  Rmethod VARCHAR,
+  CONSTRAINT "Dataset_RfactorsInfo"
+    FOREIGN KEY (modeldatapair_id) REFERENCES "ModelDataPair" (modeldatapair_id)
+);
+CREATE TABLE IF NOT EXISTS "NMRQualityData"
+  (nmrqualitydata_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL);
+CREATE TABLE IF NOT EXISTS "Crystal"(
+  crystal_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+  data_crystallographic_info_id INTEGER NOT NULL,
+  spacegroup TEXT,
+  cell_a REAL,
+  cell_b REAL,
+  cell_c REAL,
+  cell_alpha REAL,
+  cell_beta REAL,
+  cell_gamma REAL,
+  comments TEXT,
+  CONSTRAINT "DataCrystallographicInfo_Crystal"
+    FOREIGN KEY (data_crystallographic_info_id)
+      REFERENCES "DataCrystallographicInfo" (data_crystallographic_info_id)
+);
+CREATE TABLE IF NOT EXISTS "CryoEMQualityData"(
+  cryoemqualitydata_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+  SNR REAL,
+  FSC REAL,
+  SSNR REAL,
+  FNR REAL,
+  max_local_resolution REAL,
+  min_local_resolution REAL
+);
+CREATE TABLE IF NOT EXISTS "ReflectionsData"(
+  reflectionsdata_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+  data_crystallographic_info_id INTEGER NOT NULL,
+  wavelength REAL,
+  reflection_no REAL,
+  high_cell_resolution REAL,
+  low_cell_resolution REAL,
+  total_cell_resolution REAL,
+  method_resolution REAL,
+  CONSTRAINT "DataCrystallographicInfo_ReflectionsData"
+    FOREIGN KEY (data_crystallographic_info_id)
+      REFERENCES "DataCrystallographicInfo" (data_crystallographic_info_id)
+);
+CREATE TABLE CryoEMInfo(cryoem_info_id INTEGER PRIMARY KEY, dos REAL, rmsd REAL, native_overlap REAL, comments TEXT);
